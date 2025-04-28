3e4e20cf45f72066420e9c946237ad5bcd45d5ec
Alexander Kazakov <kazakovas7301@gmail.com>
2025-04-28 14:45:17 +0000

    del

--- run.sh
+++
@@ -1,3 +0,0 @@
-docker build --platform linux/amd64 -t dataset_cheetah .
-
-docker run --platform linux/amd64 -it --rm --name dataset_cheetah_v1 dataset_cheetah
--- src/agent/ppo.py
+++
@@ -1,6 +0,0 @@
-from src.agent.base import BaseRLAgent
-
-
-class PPO(BaseRLAgent):
-    def __init__(self):
-        pass
